c8a24dc31a687ef9b5257c3f4f0262e7128d5052
Mauro Cocciolo <95488234+Mauro-Cocciolo@users.noreply.github.com>
2022-05-31 16:12:40 -0300

    <strong>

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -8,9 +8,9 @@ function Testimonio() {
                 src={require("../imagenes/testimonio-emma.png")}
                 alt="Foto de Emma"/>
                 <div className="contenedor-texto-testimonio">
-                    <p className= "nombre-testimonio">Emma Bostian en Suecia</p>
-                    <p className="cargo-testimonio">Ingeniera de software en Spotify</p>
-                    <p className="texto-testimonio">"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+                    <p className= "nombre-testimonio"><strong>Emma Bostian</strong> en Suecia</p>
+                    <p className="cargo-testimonio">Ingeniera de software en <strong>Spotify</strong></p>
+                    <p className="texto-testimonio">"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on <strong>freeCodeCamp gave me the skills</strong> and confidence I needed to land my dream job as a software engineer at Spotify."</p>
 
                 </div>
         </div>
